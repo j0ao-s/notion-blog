9ebfc3706e67d501cc2ea2f4337cfe227622134e
Joao Manoel <joaaomanoeljok@gmail.com>
2025-05-05 13:17:21 -0300

    add new conteudo for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,57 +8,49 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <img
+        {/*<img
           src="/vercel-and-notion.png"
           height="85"
           width="250"
           alt="Vercel + Notion"
-        />
-        <h1>My Notion Blog</h1>
+        />*/}
+        <h1>Meu Blog Pessoal</h1>
         <h2>
-          Blazing Fast Notion Blog with Next.js'{' '}
+          Por:{' '}
           <ExtLink
-            href="https://github.com/vercel/next.js/issues/9524"
+            href="https://www.linkedin.com/in/joâo-manoel-a789781a2/"
             className="dotted"
             style={{ color: 'inherit' }}
           >
-            SSG
+            João Manoel
           </ExtLink>
         </h2>
 
-        <Features />
+        {/*<Features />*/}
 
         <div className="explanation">
           <p>
-            This is a statically generated{' '}
-            <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-            <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-            is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>
-            . It leverages some upcoming features in Next.js like{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-              SSG support
-            </ExtLink>{' '}
-            and{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-              built-in CSS support
-            </ExtLink>{' '}
-            which allow us to achieve all of the benefits listed above including
-            blazing fast speeds, great local editing experience, and always
-            being available!
+            Bem vindo ao meu Blog Pessoal, me chamo João Manoel e sou um
+            apaixonado por programação e desenvolvimento, acredito na capacidade
+            transformadora da Tecnologia, que utilizada como ferramenta pode
+            abrir muitas portas e oportunidades. Atualmente Estou concluindo
+            minha Graduação em Engenharia de Software e meu Ensino Técnico em
+            Desenvolvimento de Jogos.
           </p>
 
           <p>
-            Get started by creating a new page in Notion and clicking the deploy
-            button below. After you supply your token and the blog index id (the
-            page's id in Notion) we will automatically create the table for you!
-            See{' '}
-            <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-              here in the readme
+            Neste Blog vou documentar minha jornada, trazer minhas participações
+            em eventos, insights e muito mais sobre a área da programação. O{' '}
+            <ExtLink href="https://vercel.com/templates?type=blog&framework=next.js&search=notion">
+              Template
             </ExtLink>{' '}
-            for finding the new page's id. To get your token from Notion, login
-            and look for a cookie under www.notion.so with the name `token_v2`.
-            After finding your token and your blog's page id you should be good
-            to go!
+            usado neste Blog foi disponibilizado na loja de templates da Vercel,
+            escrito e desenvolvido por{' '}
+            <ExtLink href="https://github.com/ijjk">JJ Kasper,</ExtLink> ele é
+            desenvolvido em <ExtLink href="https://nextjs.org">Next.JS</ExtLink>{' '}
+            e integrado ao{' '}
+            <ExtLink href="https://www.notion.com">Notion,</ExtLink> onde o
+            utilizo como CMS.
           </p>
         </div>
       </div>
